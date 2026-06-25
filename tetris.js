2e638f4c4440e4de45ee2f7347e72cb405278056
lowrderrr <165852321+lowrderrr@users.noreply.github.com>
2026-06-25 13:40:07 +0200

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,23 +1,32 @@
 class Tetris {
-    constructor (imageX,imageY,template) {
+    constructor (imageX, imageY, template) {
 
-        this.imageY=imageY;
-        this.imageX=imageX;
-        this.template=template;
+        this.imageY = imageY;
+        this.imageX = imageX;
+        this.template = template;
     }
-   }
+    checkBottom(){}
+    checkLeft(){}
+    checkRight(){}
+    moveRight(){}
+    changeRotation(){}
+}
 
+const imageSquaresize = 24;
 const size = 40;
-const framePerSecond = 24; 
+const FramePerSecond = 24;
 const gameSpeed = 5;
 const canvas = document.getElementById("canvas");
+const image = document.getElementById("images");
 const ctx = canvas.getContext("2d");
+const squareCountX = canvas.width / size;
+const squareCountY = canvas.height / size;
 
 const shapes =[
-    new Tetris(0,120,[
+    new tetris(0,120,[
         [0,1,0],
-        [0,1,1],
-        [1,1,0],
+        [0.1,1],
+        [1.1,0],
     ]),
     new Tetris(0.96,[
         [0,0,0],
@@ -55,32 +64,74 @@ const shapes =[
     ]),
 ];
 
-let currentShape = shapes[0];
+let getMap;
+let gameOver;
+let CurrentShape;
+let NextShape;
+let score;
+let initialTwoDarr;
 
-function drawBackground() {
+let gameLoop = () => {
+
+setInterval(update, 1000 / gameSpeed);
+setInterval(draw, 1000 / framePerSecond);
+};
+
+let update = () => {};
+
+letdrawRect = (x,y)
+
+let currentDrawTetris = () => {
+    for (let i = 0; i< CurrentShape.template.length; i++) {
+        for (let j = 0; j< CurrentShape.template[i].length; j++) {
+            if (CurrentShape.template[i][j] === 0) continue;
+            ctx.drawImage(
+                image,
+                currentShape.imageX + j * imageSquaresize,
+                currentShape.imageY + i * imageSquaresize,
+                imageSquaresize,
+                imageSquaresize,
+                Math.trunc(CurrentShape.x + j) * size,
+                Math.trunc(CurrentShape.y + i) * size,
+
+            );
+        }
+    };
+};
+
+let drawingShape = () =>{};
+
+let draw = () => {
+    ctx.clearReact(0, 0, canvas.width, canvas.height);
+    drawBackground();
+    drawSquare();
+    currentDrawTetris();
+    drawNextShape();
+    if (gameOver) {
+        drawGameOver();
+    }
+};
+
+let drawBackground = () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+};
 
-function currentDrawTetris() {
-    for (let i= 0; i < currentShape.template.length; i++) {
-        for (;et j = 0; j < currentShape.template[i][j] === 0) continue;
-        ctx.fillStyle = "lime";
-        ctx.fillRect(
-            currentShape.imageX + j * size,
-            currentShape.imageY + i * size, 
-            size, 
-            size, 
-       );
+let getRandomShape = () => {
+    return Object.create(shapes [ Math.floor ( Math.random () * shapes.length)]);
+};
+
+let resetVars = () => {
+    initialTwoDarr = [];
+    for (let i = 0; i < squareCountY; i++) {
+        let temp= [];
+        for(let j = 0; j < squareCountX; j++) {
+            temp.push({imageX: -1, imageY: -1});
+        }
+        initialTwoDarr.push(temp);
     }
-  }
+    score = 0;
+    gameOver = false;
+    currentShape = getRandomShape();
+    nextShape = getRandomShape();
 }
-
-function draw() {
-    drawBackground();
-    currentDrawTetris();
-}
-
-setInterval(draw, 1000 / framePerSecond); 
-
-
